818ea4f388527ec6e473d23340422e279bf6d915
thanhtrung08092000 <72597692+thanhtrung08092000@users.noreply.github.com>
2021-09-29 20:42:07 +0700

    update

--- app/src/main/java/com/trung/cuahangonline/adapter/DonghocasioAdapter.java
+++ app/src/main/java/com/trung/cuahangonline/adapter/DonghocasioAdapter.java
@@ -2,6 +2,7 @@ package com.trung.cuahangonline.adapter;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 import com.trung.cuahangonline.R;
 import com.trung.cuahangonline.model.Sanpham;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 public class DonghocasioAdapter extends BaseAdapter {
     Context context;
     ArrayList<Sanpham> arraydongho;
+    private View view;
 
     public DonghocasioAdapter(Context context, ArrayList<Sanpham> arraydongho) {
         this.context = context;
@@ -45,12 +48,13 @@ public class DonghocasioAdapter extends BaseAdapter {
 
     }
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int i, View View, ViewGroup parent) {
         ViewHolder viewHolder= null;
+        view = View;
         if(view == null){
             viewHolder = new ViewHolder();
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.dong_donghocasio,null);
+            view = inflater.inflate(R.layout.dong_donghocasio,parent,false);
 
             viewHolder.txttendonghocasio= view.findViewById(R.id.textviewdonghocasio);
             viewHolder.txtgiadonghocasio= view.findViewById(R.id.textviewgiasanpham);
@@ -60,15 +64,22 @@ public class DonghocasioAdapter extends BaseAdapter {
         }else {
             viewHolder = (ViewHolder) view.getTag();
         }
+        Log.d("adapter", "getView: "+arraydongho.size());
         Sanpham sanpham= (Sanpham) getItem(i);
         viewHolder.txttendonghocasio.setText(sanpham.getTensanpham());
-        //DecimalFormat decimalFormat= new DecimalFormat("###,###,###");
-       // viewHolder.txtgiadonghocasio.setText("Gía :"+decimalFormat.format(sanpham.getGiasanpham()) +"Đ"  );
+
+
+        DecimalFormat decimalFormat= new DecimalFormat("###,###,###");
+       //viewHolder.txtgiadonghocasio.setText("Giá : " + decimalFormat.format(sanpham.getGiasanpham())+ "Đ");
+
+
        viewHolder.txtmotadonghocasio.setMaxLines(2);
         viewHolder.txtmotadonghocasio.setEllipsize(TextUtils.TruncateAt.END);
         viewHolder.txtmotadonghocasio.setText(sanpham.getMotasanpham());
-       Picasso.with(context).load(sanpham.getHinhanhsanpham())
+//ban dung co sai picaso no cu roi
+       Glide.with(context).load(sanpham.getHinhanhsanpham())
                .into(viewHolder.imgdonghocasio);
         return view;
     }
+
 }
