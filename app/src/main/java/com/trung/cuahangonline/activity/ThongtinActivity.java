335960d797043c162b7956760ccb168c4f0793fe
thanhtrung08092000 <72597692+thanhtrung08092000@users.noreply.github.com>
2021-10-05 00:45:38 +0700

    update

--- app/src/main/java/com/trung/cuahangonline/activity/ThongtinActivity.java
+++ app/src/main/java/com/trung/cuahangonline/activity/ThongtinActivity.java
@@ -1,16 +1,109 @@
 package com.trung.cuahangonline.activity;
 
+import static com.trung.cuahangonline.utils.Server.duongdandonhang;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.trung.cuahangonline.R;
+import com.trung.cuahangonline.utils.CheckConnection;
+import com.trung.cuahangonline.utils.Server;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ThongtinActivity extends AppCompatActivity {
+    EditText edittenkhachhang, editemail, editsdt;
+    Button btnxacnhan,btntrove;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thongtin);
+        Anhxa();
+
+        btntrove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+            EventButton();
+        }else {
+            CheckConnection.ShowToast_Short(getApplicationContext(),"Bạn hãy kiểm tra lại kết nối");
+        }
     }
+
+    private void EventButton() {
+        btnxacnhan.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                final String ten = edittenkhachhang.getText().toString().trim();
+                final String sdt = editsdt.getText().toString().trim();
+                final String email = editemail.getText().toString().trim();
+                if(ten.length()>0 && sdt.length()>0 && email.length()>0){
+                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                    StringRequest stringRequest= new StringRequest(Request.Method.POST, duongdandonhang, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("madonhang",response);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }){
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            HashMap<String,String> hashMap = new HashMap<String,String>();
+                            hashMap.put("tenkhachhang",ten);
+                            hashMap.put("sodienthoai",sdt);
+                            hashMap.put("email",ten);
+
+
+
+                            return hashMap;
+                        }
+                    };
+                    requestQueue.add(stringRequest);
+                }else {
+                    CheckConnection.ShowToast_Short(getApplicationContext(),"Đã xảy ra lỗi! Đặt hàng chưa thành công");
+                }
+
+            }
+        });
+
+    }
+
+    private void Anhxa() {
+        edittenkhachhang = findViewById(R.id.edttexttenkhachhang);
+        editemail = findViewById(R.id.edittextemail);
+        editsdt = findViewById(R.id.editsodienthoai);
+        btnxacnhan = findViewById(R.id.buttonxacnhan);
+        btntrove = findViewById(R.id.buttontrove);
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/trung/cuahangonline/utils/Server.java
+++ app/src/main/java/com/trung/cuahangonline/utils/Server.java
@@ -3,8 +3,9 @@ package com.trung.cuahangonline.utils;
 public class Server {
     public static String localhost="192.168.1.5";
     public static String duongdanloaisp = "http://" + localhost + "/server/getloaisanpham.php";
-    public static String duongdansanphammoinhat = "http://" + localhost + "/server/getsanphammoinhat.php";
+    public static String duongdansanphammoinhat = "http://" + localhost + "/server/getsanphamnoibat.php";
     public static String duongdandonghocasio = "http://" + localhost + "/server/getsanpham.php?page=";
+    public static String duongdandonhang = "http://" + localhost + "/server/thongtinkhachhang.php";
 
 
 
